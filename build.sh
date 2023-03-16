5f5937e4e754c84e263e95cf3d9ccbae3ab6a33c
zehara <107530847+zehara19@users.noreply.github.com>
2023-03-16 14:18:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,28 @@
-#! /bin/bash
-set -e
-chmod +x build.sh
-docker stop $(docker ps -aq) || true
-docker rm $(docker ps -aq) || true
-docker build -t reactjs-demo .
+#!/bin/bash
+cat <<EOT >Dockerfile
+FROM node:16-alpine as build
+WORKDIR /rohithbuildapp
+COPY package*.json /rohithbuildapp/
+RUN npm install
+COPY . .
+RUN npm run build
+FROM nginx:latest
+COPY --from=build /rohithbuildapp/build/ /usr/share/nginx/html/
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
+EOT
+
+echo "Completed creating docker file. Below are its contents"
+
+cat Dockerfile
+
+
+echo "Now we will create an image using this docker file with when function using jenkins "
+
+docker build -t rohithsgr/dev:${BUILD_NUMBER} .
+
+
+
+
+
+
