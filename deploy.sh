1ba54485dd51f703675e1485793633c2363cc156
zehara <107530847+zehara19@users.noreply.github.com>
2023-03-16 14:18:48 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
-chmod +x deploy.sh
-docker-compose up
-docker ps -a
+#!/bin/bash
+echo "Please find below list of images"
+docker images
+echo "Now we will build container of the image and run it on 9090 port of the host machine"
+docker run -d -p 9090:80 rohithbuildapp:latest
